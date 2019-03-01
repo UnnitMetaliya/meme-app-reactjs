c5dc2d97d1f211723b196700b81ac70fed1b1eca
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-02-28 20:04:29 -0500

    Updates

---
+++ precache-manifest.2474d2263c394daf97af8dca77134a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c7dad1c07452147c245",
+    "url": "/meme-app-reactjs/static/js/runtime~main.9c7dad1c.js"
+  },
+  {
+    "revision": "d85e7bf1a975269efb09",
+    "url": "/meme-app-reactjs/static/js/main.d85e7bf1.chunk.js"
+  },
+  {
+    "revision": "8122262c9f7998fa8696",
+    "url": "/meme-app-reactjs/static/js/2.8122262c.chunk.js"
+  },
+  {
+    "revision": "d85e7bf1a975269efb09",
+    "url": "/meme-app-reactjs/static/css/main.e6440a1d.chunk.css"
+  },
+  {
+    "revision": "8122262c9f7998fa8696",
+    "url": "/meme-app-reactjs/static/css/2.50ed4123.chunk.css"
+  },
+  {
+    "revision": "ebec4a4058ca301d93d2d95ba1cd76c6",
+    "url": "/meme-app-reactjs/index.html"
+  }
+];
